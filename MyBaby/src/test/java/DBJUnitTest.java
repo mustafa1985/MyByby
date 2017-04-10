fed713af92720f17e30c400c260d29cd613b8a00
mustafa1985 <mustafa200485@yahoo.com>
2017-04-11 02:22:05 +0300

    add test

---
+++ MyBaby/src/test/java/DBJUnitTest.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import com.MyBaby.bean.Baby;
+import com.MyBaby.dao.BabyDao;
+import com.mysql.jdbc.Connection;
+import java.sql.SQLException;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author satwa
+ */
+public class DBJUnitTest {
+
+    private Connection connection;
+
+    @Before
+    public void setUp() {
+        connection = (Connection) BabyDao.getCon();
+    }
+
+    @After
+    public void tearDown() throws SQLException {
+        connection.close();
+    }
+
+    @Test
+    public void addNewBaby() {
+        Baby baby = new Baby();
+        baby.setName("Ahmed");
+        baby.setReligion("Muslims");
+        baby.setSex("Male");
+        baby.setMeaning("OFFFFF");
+        BabyDao.save(baby);
+    }
+
+    @Test
+    public void deleteBaby() {
+        BabyDao.delete(1);
+    }
+
+    @Test
+    public void allBabies() {
+//        assertEquals(9, BabyDao.getAllRecords().size()); 
+    }
+
+}
