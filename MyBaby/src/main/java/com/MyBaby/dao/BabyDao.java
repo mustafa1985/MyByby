8b14d48e1912f9ef6dd59f42c55293af44294eef
hadeel1978 <hadeel_faroq@yahoo.com>
2017-04-11 02:55:39 +0300

    improveing the code

--- MyBaby/src/main/java/com/MyBaby/dao/BabyDao.java
+++ MyBaby/src/main/java/com/MyBaby/dao/BabyDao.java
@@ -11,9 +11,10 @@ public class BabyDao {
         Connection con = null;
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "mustafa");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
         } catch (Exception e) {
             System.out.println(e);
+           // System.out.println(e);
         }
         return con;
     }
